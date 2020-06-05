aabc9575c6b351195116dc50bfec33c01adbffe2
Tim Martin <tim@asymptotic.co.uk>
2020-06-05 11:11:28 +0100

    Format the JS according to the wp-scripts standard config
    This includes using tabs for indentation, which really sucks, but a
    convention is a convention.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,43 @@
 import { registerBlockType } from '@wordpress/blocks';
 import domReady from '@wordpress/dom-ready';
 
-import {Tokenizer} from "@timmartin2/python-code-analyzer";
-import "@timmartin2/python-code-analyzer/lib/main.css";
+import { Tokenizer } from '@timmartin2/python-code-analyzer';
+import '@timmartin2/python-code-analyzer/lib/main.css';
 
-registerBlockType('wp-python-analyzer/tokenizer', {
-  title: 'Python interactive tokenizer',
-  icon: 'media-code',
-  category: 'embed',
-  attributes: {
-    source: {
-      type: 'string',
-      source: 'text',
-      selector: 'div.wp-python-analyzer-tokenizer'
-    }
-  },
-  edit: ({attributes, setAttributes}) => (
-    <textarea value={attributes.source} onChange={(e) => { setAttributes({source: e.target.value}); }} />
-  ),
-  save: ({attributes}) => <div className="wp-python-analyzer-tokenizer">{attributes.source}</div>,
-});
+registerBlockType( 'wp-python-analyzer/tokenizer', {
+	title: 'Python interactive tokenizer',
+	icon: 'media-code',
+	category: 'embed',
+	attributes: {
+		source: {
+			type: 'string',
+			source: 'text',
+			selector: 'div.wp-python-analyzer-tokenizer',
+		},
+	},
+	edit: ( { attributes, setAttributes } ) => (
+		<textarea
+			value={ attributes.source }
+			onChange={ ( e ) => {
+				setAttributes( { source: e.target.value } );
+			} }
+		/>
+	),
+	save: ( { attributes } ) => (
+		<div className="wp-python-analyzer-tokenizer">
+			{ attributes.source }
+		</div>
+	),
+} );
 
-domReady(() => {
-	const tokenizerInstances = document.getElementsByClassName('wp-python-analyzer-tokenizer');
-	for (const tokenizer of tokenizerInstances) {
+domReady( () => {
+	const tokenizerInstances = document.getElementsByClassName(
+		'wp-python-analyzer-tokenizer'
+	);
+	for ( const tokenizer of tokenizerInstances ) {
 		ReactDOM.render(
-      <Tokenizer initialCode={tokenizer.textContent} />,
-      tokenizer
-    );
+			<Tokenizer initialCode={ tokenizer.textContent } />,
+			tokenizer
+		);
 	}
-});
+} );
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
-  ...defaultConfig,
-  plugins: [...defaultConfig.plugins, new MiniCssExtractPlugin()],
-  module: {
-    ...defaultConfig.module,
-    rules: [
-      ...defaultConfig.module.rules,
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: true,
-            },
-          },
-          "css-loader",
-        ],
-      },
-    ],
-  },
+	...defaultConfig,
+	plugins: [ ...defaultConfig.plugins, new MiniCssExtractPlugin() ],
+	module: {
+		...defaultConfig.module,
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							esModule: true,
+						},
+					},
+					'css-loader',
+				],
+			},
+		],
+	},
 };
