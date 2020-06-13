f32df1a0d0a19a5065fe3f0e79fbd90ad97510bf
Tim Martin <tim@asymptotic.co.uk>
2020-06-13 14:02:52 +0100

    Set version number to 0.0.5

--- class-wp-python-analyzer.php
+++ class-wp-python-analyzer.php
@@ -2,7 +2,7 @@
 /*
  * Plugin name: Python code analyzer
  * Description: Embed widgets for live analysis of Python code
- * Version: 0.0.4
+ * Version: 0.0.5
  */
 
 function python_analyzer_register_block() {
